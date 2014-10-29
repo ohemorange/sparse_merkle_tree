2467bc0d00b3dfadaca1f7948fa759a85f4081c0
Erica Portnoy <eportnoy@princeton.edu>
2014-10-29 21:56:50 +0100

    usage example in servery.py

--- server.py
+++ server.py
@@ -10,3 +10,11 @@ def check_proof_against_root(k, v, n, proof_output, root):
 def audit_smt(l, n, k, v, prev_root):
     proof_output = proof(l, n, k)
     return check_proof_against_root(k, v, n, proof_output, prev_root)
+
+def list_example():
+    usernames = ["ohemorange", "joebonneau", "edfelten", "bcrypt"]
+    hashed_usernames = [hex_to_int(Hash(user)) for user in usernames]
+    pks = [str(i) for i in [10,3,6,100]]
+    l = zip(hashed_usernames, pks)
+    result = construct(256, l)
+    assert audit_smt(l, 256, hex_to_int(Hash('ohemorange')), 10, result)
